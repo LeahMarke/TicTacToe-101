f5d726a13440ae433b4044c680140f79917d1ae7
leahmarke <lmarke@live.com>
2022-06-03 01:59:51 -0400

    just started

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 //       ***********************
 //            INSTRUCTIONS
 //       ***********************
+let board = [
+  ["","",""],
+  ["","",""],
+  ["","",""]
+]
+
 let currentMarker = "X";
 
 const handleClick = (element) => {
@@ -17,6 +23,7 @@ const addMarker = (id) => {
   );
   document.getElementById(id).innerHTML = currentMarker;
   changeMarker();
+  board[row][column] = currentMarker
 };
 
 const changeMarker = () => {
@@ -34,3 +41,52 @@ const resetBoard = () => {
     squares[i].innerHTML = null;
   }
 };
+
+// -----------TTT Part 2-------------
+
+
+
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
+
+const horizontalWin = () => {
+  // Your code here to check for horizontal wins
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+        || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+    )
+  if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+        || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+    )
+  if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+        || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
+    )
+
+}
+
+const verticalWin = () => {
+  // Your code here to check for vertical wins
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+        || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+    )
+  if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+        || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+    )
+  if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+        || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
+    )
+}
+
+const diagonalWin = () => {
+  // Your code here to check for diagonal wins
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+        || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+    )
+  if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+        || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
+    )
+}
