a62eeac86dde6893696ea37ef3e1d1a5e09e2d41
leahmarke <lmarke@live.com>
2022-06-04 23:14:46 -0400

    added stuff but have no clue what to do

--- scripts.js
+++ scripts.js
@@ -2,15 +2,22 @@
 //            INSTRUCTIONS
 //       ***********************
 let board = [
-  ["","",""],
-  ["","",""],
-  ["","",""]
-]
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
 
 let currentMarker = "X";
 
 const handleClick = (element) => {
+  const row = parseInt(element.id.charAt(0));
+  const column = parseInt(element.id.charAt(2));
+  row = 0;
+  column = 0;
+  board[row][column] = currentMarker;
+  board[0][0] = "X";
   console.log(`The element you clicked on has an id:  ${element.id}`);
+
   if (!document.getElementById(element.id).innerHTML) {
     addMarker(element.id);
   }
@@ -21,9 +28,9 @@ const addMarker = (id) => {
   console.log(
     `Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`
   );
-  document.getElementById(id).innerHTML = currentMarker;
-  changeMarker();
-  board[row][column] = currentMarker
+  // document.getElementById(id).innerHTML = currentMarker;
+  checkForWin();
+  // changeMarker();
 };
 
 const changeMarker = () => {
@@ -44,49 +51,54 @@ const resetBoard = () => {
 
 // -----------TTT Part 2-------------
 
-
-
 const checkForWin = () => {
-  if(horizontalWin() || verticalWin() || diagonalWin()) {
-    window.alert(`Player ${currentMarker} won!`)
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`);
   } else {
-    changeMarker()
+    changeMarker();
   }
-}
+};
 
 const horizontalWin = () => {
   // Your code here to check for horizontal wins
-  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
-        || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
-    )
-  if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
-        || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
-    )
-  if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
-        || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
-    )
-
-}
+  if (
+    (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") ||
+    (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+  );
+  if (
+    (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") ||
+    (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+  );
+  if (
+    (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") ||
+    (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
+  );
+};
 
 const verticalWin = () => {
   // Your code here to check for vertical wins
-  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
-        || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
-    )
-  if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
-        || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
-    )
-  if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
-        || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
-    )
-}
+  if (
+    (board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") ||
+    (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+  );
+  if (
+    (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") ||
+    (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+  );
+  if (
+    (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") ||
+    (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
+  );
+};
 
 const diagonalWin = () => {
   // Your code here to check for diagonal wins
-  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
-        || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
-    )
-  if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
-        || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
-    )
-}
+  if (
+    (board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") ||
+    (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+  );
+  if (
+    (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") ||
+    (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
+  );
+};
